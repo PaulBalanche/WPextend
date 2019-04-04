7237dc7e69675f6f13b870ace5ac354253545962
paul.balanche <pb@buzzbrothers.ch>
2019-04-04 11:11:16 +0200

    Update 2.2.0

--- wpextend.php
+++ wpextend.php
@@ -4,7 +4,7 @@
 * Plugin URI: https://github.com/PaulBalanche/WPextend
 * Description: Extends basic Wordpress features such as add general settings, easy creating custom post type, ...
 * Text Domain: wp-extend
-* Version: 2.1.99
+* Version: 2.2.0
 * Author: Paul Balanche
 **/
 
