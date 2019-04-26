6f44db9dd2c9e9088fc70295b9c8939cedfd0b2f
paul.balanche <pb@buzzbrothers.ch>
2019-04-26 11:56:17 +0200

    Add thumbnail API  browser caching

--- wpextend.php
+++ wpextend.php
@@ -4,7 +4,7 @@
 * Plugin URI: https://github.com/PaulBalanche/WPextend
 * Description: Extends basic Wordpress features such as add general settings, easy creating custom post type, ...
 * Text Domain: wp-extend
-* Version: 2.2.7
+* Version: 2.2.8
 * Author: Paul Balanche
 **/
 
