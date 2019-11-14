735a2ef1e678777f0be319581b81bef5fb724f78
paul.balanche <pb@buzzbrothers.ch>
2019-11-14 17:13:34 +0100

    Improve ThumbnailApi support in order to copy images need the first time and then called statically access images

--- inc/classes/ThumbnailApi.php
+++ inc/classes/ThumbnailApi.php
@@ -14,7 +14,8 @@ class ThumbnailApi {
      */
     private static $_instance;
 
-    public $path_base_thumbnail_api = 'images';
+    public $path_base_thumbnail_api = 'images',
+        $path_generated_images = 'images-generated';
 
 
     /**
@@ -38,7 +39,8 @@ class ThumbnailApi {
     private function __construct() {
 
         // Add new rewrite rule to allow thumbnail api
-        add_action( 'init', array($this, 'rewrite_thumbnail_url') );
+        // add_action( 'init', array($this, 'rewrite_thumbnail_url') );
+        add_filter( 'mod_rewrite_rules', array($this, 'rewrite_thumbnail_url') );
 
         // Attach admin post action to catch thumbnail_url rewritted
         add_action( 'admin_post_' . WPEXTEND_MAIN_SLUG_ADMIN_PAGE . '_thumbnail_api', array($this, 'catch_thumbnail_url_rewritted') );
@@ -46,14 +48,55 @@ class ThumbnailApi {
     }
 
 
-
     /**
      * Add new rewrite rule to allow thumbnail api
      * 
      */
-    public function rewrite_thumbnail_url() {
+    function rewrite_thumbnail_url( $rewrite ) {
 
-        add_rewrite_rule( $this->path_base_thumbnail_api . '/(.+)$', str_replace(home_url() . '/', '', admin_url()) . 'admin-post.php?action=' . WPEXTEND_MAIN_SLUG_ADMIN_PAGE . '_thumbnail_api&name_image=$1', 'top');
+        // Step 1
+        $new_rules = "\n# WP Extend ThumbnailApi support step 1\n";
+        $new_rules .= "<IfModule mod_rewrite.c>\n";
+
+        $new_rules .= "RewriteEngine On\n";
+        $new_rules .= "RewriteBase /\n";
+
+        $new_rules .= "# URL contains width and height query string\n";
+        $new_rules .= "RewriteCond %{REQUEST_URI} ^/" . $this->path_base_thumbnail_api . "/\n";
+        $new_rules .= "RewriteCond %{QUERY_STRING} ^w=([0-9]*)&h=([0-9]*)$\n";
+        $new_rules .= "RewriteRule ^" . $this->path_base_thumbnail_api . "/(.*)$ /" . $this->path_generated_images . "/w%1-h%2-$1? [QSA,L]\n";
+
+        $new_rules .= "# URL contains width only query string\n";
+        $new_rules .= "RewriteCond %{REQUEST_URI} ^/" . $this->path_base_thumbnail_api . "/\n";
+        $new_rules .= "RewriteCond %{QUERY_STRING} ^w=([0-9]*)$\n";
+        $new_rules .= "RewriteRule ^" . $this->path_base_thumbnail_api . "/(.*)$ /" . $this->path_generated_images . "/w%1-h0-$1? [QSA,L]\n";
+
+        $new_rules .= "# URL contains height only query string\n";
+        $new_rules .= "RewriteCond %{REQUEST_URI} ^/" . $this->path_base_thumbnail_api . "/\n";
+        $new_rules .= "RewriteCond %{QUERY_STRING} ^h=([0-9]*)$\n";
+        $new_rules .= "RewriteRule ^" . $this->path_base_thumbnail_api . "/(.*)$ /" . $this->path_generated_images . "/w0-h%1-$1? [QSA,L]\n";
+
+        $new_rules .= "# URL does not contains any query string\n";
+        $new_rules .= "RewriteCond %{REQUEST_URI} ^/" . $this->path_base_thumbnail_api . "/\n";
+        $new_rules .= "RewriteRule ^" . $this->path_base_thumbnail_api . "/(.*)$ /" . $this->path_generated_images . "/w0-h0-$1? [QSA,L]\n";
+
+        $new_rules .= "</IfModule>\n\n";
+
+        // Step 2
+        $new_rules .= "# WP Extend ThumbnailApi support step 2\n";
+        $new_rules .= "<IfModule mod_rewrite.c>\n";
+
+        $new_rules .= "RewriteEngine On\n";
+        $new_rules .= "RewriteBase /" . $this->path_generated_images . "/\n";
+  
+        $new_rules .= "# redirect all requests to non-existing resources to special handler\n";
+        $new_rules .= "RewriteCond %{REQUEST_FILENAME} !-f\n";
+        $new_rules .= "RewriteCond %{REQUEST_FILENAME} !-d\n";
+        $new_rules .= "RewriteRule ^" . $this->path_generated_images . "/(.+)$ /" . str_replace(home_url() . '/', '', admin_url()) . "admin-post.php?action=" . WPEXTEND_MAIN_SLUG_ADMIN_PAGE . "_thumbnail_api&name_image=$1 [QSA,L]\n";
+
+        $new_rules .= "</IfModule>\n\n";
+
+        return $new_rules . $rewrite;
     }
 
 
@@ -64,8 +107,8 @@ class ThumbnailApi {
      */
     public function catch_thumbnail_url_rewritted() {
 
-        if( isset($_GET['name_image']) && !empty($_GET['name_image']) && preg_match('/^(\d+)-(.*)$/', $_GET['name_image'], $matches) && is_array($matches) && count($matches) > 1 && is_numeric($matches[1]) ) {
-            
+        if( isset($_GET['name_image']) && !empty($_GET['name_image']) && preg_match('/^w(\d+)-h(\d+)-(\d+)-(.*)$/', $_GET['name_image'], $matches) && is_array($matches) && count($matches) > 1 && is_numeric($matches[3]) ) {
+
             $size_image = 'large';
 
             $consider_width = ( isset($_GET['w']) && is_numeric($_GET['w']) );
@@ -73,7 +116,7 @@ class ThumbnailApi {
             
             if( $consider_width || $consider_height ) {
                 
-                $attachment_metadata = wp_get_attachment_metadata($matches[1]);
+                $attachment_metadata = wp_get_attachment_metadata($matches[3]);
                 $sizes_available = $attachment_metadata['sizes'];
                 if( !isset($sizes_available['large']) ) {
                     $sizes_available['large'] = [
@@ -141,13 +184,19 @@ class ThumbnailApi {
             }
             
             // Render image
-            $data_image = wp_get_attachment_image_src($matches[1], $size_image);
+            $data_image = wp_get_attachment_image_src($matches[3], $size_image);
             if( $data_image && is_array($data_image) && count($data_image) > 0 ) {
 
                 // Get image PATH
                 $wp_upload_dir = wp_upload_dir();
                 $path_image = str_replace($wp_upload_dir['baseurl'], $wp_upload_dir['basedir'], $data_image[0]);
                 if( file_exists($path_image) ) {
+
+                    // Copy image
+                    $name_directory_image_copy = dirname(ABSPATH) . '/' . $this->path_generated_images . '/';
+                    if( !file_exists($name_directory_image_copy) )
+                        mkdir($name_directory_image_copy);
+                    copy( $path_image, $name_directory_image_copy . $matches[0] );
 
                     $last_modified_time = filemtime($path_image);
                     $cache_duration = 3600 * 24 * 30; // 30 days...
@@ -174,11 +223,6 @@ class ThumbnailApi {
                                 'verify_peer'   => false
                             ]
                         ]));
-                        // echo file_get_contents($path_image, false, stream_context_create([
-                        //     'ssl' => [
-                        //         'verify_peer'   => false
-                        //     ]
-                        // ]));
                     }
                 }
             }
